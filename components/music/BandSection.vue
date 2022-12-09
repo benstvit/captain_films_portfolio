198f895210f6d3a5ff835ab86b73378ffafbfcd0
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-12-09 15:54:55 +0100

    music page WIP

--- components/music/BandSection.vue
+++ components/music/BandSection.vue
@@ -1,16 +1,19 @@
 <template>
   <section>
     <div
-      class="p-4 h-screen rounded-lg bg-yellow-50">
+      class="flex flex-col justify-center p-4 h-full rounded-lg bg-yellow-50">
       <div
         id="bandBanner"
-        class="w-2/3 h-2/3 my-12">
-        <nuxt-img :src="this.photos[0].url" :alt="this.photos[0].title"/>
+        class="flex justify-around items-start my-12 mx-4">
+        <nuxt-img
+          :src="this.photos[0].url"
+          :alt="this.photos[0].title"
+          class="rounded-full h-96"/>
+          <h1 class="items-start font-playfair text-5xl">NAVAHO</h1>
       </div>
       <div
         id="bandHorizontalScroll"
-        class="flex justify-between items-center gap-24 scrollbar-hide overflow-x-scroll scroll-auto scroll-p-0 scroll-m-0 px-12">
-        <h1 class="whitespace-nowrap font-captainbold uppercase text-4xl font-bold">Ben And The Saints</h1>
+        class="flex justify-between items-center gap-24 scrollbar-hide overflow-x-scroll scroll-auto scroll-p-0 scroll-m-0 my-6 mx-4 px-12">
         <nuxt-img
           class="w-96 h-72 bg-black rounded-full"
           src="@/static/logo-solo.png">
@@ -69,7 +72,6 @@ export default {
     }
   },
   mounted () {
-    console.log(this.photos);
     const horizontalScroll = document.getElementById('bandHorizontalScroll');
     this.bandHorizontalScroll = horizontalScroll;
     this.bandHorizontalScroll.addEventListener('wheel', this.scrollHorizontaly);
