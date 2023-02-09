b1bb10ff6083adc8ad85b09ca624056b19b39f73
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-02-09 13:53:54 +0100

    fade in effect showroom

--- components/showroom/ShowroomGallery.vue
+++ components/showroom/ShowroomGallery.vue
@@ -4,14 +4,20 @@
       <div
         v-for="photo in photos"
         :key="photo.title"
-        class="my-4 py-2"
+        class="my-4 py-4"
+        :data-aos="photo.index !== 1 && 'fade-in'"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
         :class="photo.index % 2 === 0 ? 'self-center lg:self-end mx-2 lg:mx-0 lg:mr-12' : 'self-center lg:self-start mx-2 lg:mx-0 lg:ml-12'">
         <viewer>
           <keep-alive>
             <nuxt-img
               v.prlx.mobile
-              v-prlx.background="{ speed: 0.07 }"
-              v-prlx ="{ speed: 0.1}"
+              v-prlx="{
+                speed: 0.1,
+                preserveInitialPosition: false,
+                limit: { min: -30, max: 60 }
+              }"
               class="h-[50vh] object-cover rounded-md shadow-md hover:shadow-lg xl:hover:opacity-80 hover:cursor-zoom-in"
               :src="photo.url"
               :alt="photo.title"/>
@@ -24,9 +30,11 @@
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import aos from "../../mixins/aos"
 
 export default {
   name: 'ShowroomGallery',
+  mixins: [aos],
   computed: {
     ...mapState('showroom', { showroomData: 'data'}),
 
