b672e433803c0bc743a78f62edf73ad66041ea42
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-02-21 14:26:32 +0100

    vertical text for menu

--- components/partials/CaptainFilmsLogo.vue
+++ components/partials/CaptainFilmsLogo.vue
@@ -1,29 +1,30 @@
 <template>
-  <!-- <div
-    id="captain-logo"
-    v-if="menuDisplay"
-    class="flex flex-col justify-between items-center mx-2 lg:w-44 xl:w-36 text-center">
-      <span class="justify-self-start">
-        <nuxt-img
-          class="md:h-20 md:w-20 lg:h-28 lg:w-28"
-          src="/logo-solo.png">
-        </nuxt-img>
-        <h1 class="font-captainbold font-bold text-center text-sm text-black">
-          Captain Films
-        </h1>
-        <p class="font-captainlight text-center italic text-xs pb-4 text-black">
-          Film Photography
-        </p>
-      </span>
-  </div> -->
    <section>
-     <div
-      class="w-32 flex col justify-center"
+     <!-- <div
+      class="w-32 flex-col justify-center"
       v-if="menuDisplay">
       <h1 class="hidden lg:block md:text-2xl lg:text-4xl text-gray-800 uppercase whitespace-nowrap rotate-90 font-cormorant tracking-widest">
         Captain Films
       </h1>
-     </div>
+     </div> -->
+      <div class="w-24 flex flex-col justify-center items-center gap-6">
+        <div class="flex-col justify-center">
+          <h1
+            v-for="letter in 'Captain'"
+            class="text-center py-1 hidden lg:block md:text-2xl lg:text-4xl text-gray-800 whitespace-nowrap uppercase font-cormorant"
+            :key="letter">
+              {{letter}}
+          </h1>
+        </div >
+        <div class="flex-col justify-center items-center">
+          <h1
+            v-for="letter in 'Films'"
+            class="text-center py-1 hidden lg:block md:text-2xl lg:text-4xl text-gray-800 whitespace-nowrap uppercase font-cormorant"
+            :key="letter">
+              {{letter}}
+          </h1>
+        </div >
+      </div >
    </section>
 </template>
 
