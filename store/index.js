132a2b1b1883a3db205e604d017cd83c48f47541
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-02-23 13:19:45 +0100

    form validation for message

--- store/index.js
+++ store/index.js
@@ -2,6 +2,7 @@ import 'viewerjs/dist/viewer.css'
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import VueAnimateOnScroll from 'vue-animate-onscroll'
 import VueFormulate from '@braid/vue-formulate'
 import VuePrlx from 'vue-prlx'
 import VueViewer from 'v-viewer'
@@ -10,18 +11,10 @@ import banner from "./modules/banner.js";
 import photography from "./modules/photography.js";
 import showroom from "./modules/showroom.js";
 
-import Vue2TouchEvents from 'vue2-touch-events'
-
-const viewerOptions = {
-  toolbar: false,
-  button: true
-}
-
-
-Vue.use(Vue2TouchEvents)
+Vue.use(VueAnimateOnScroll);
 Vue.use(VueFormulate);
 Vue.use(VuePrlx);
-Vue.use(VueViewer, viewerOptions)
+Vue.use(VueViewer);
 Vue.use(Vuex);
 
 export default () => new Vuex.Store({
