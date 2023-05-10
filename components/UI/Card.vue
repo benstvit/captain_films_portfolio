2d49158e0850f32292184b38ddaecd0ad065c89b
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-05-10 14:31:14 +0200

    Working on design of index page

---
+++ components/UI/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nuxt-img
+      preload
+      format="webp"
+      class="shadow-md rounded-md"
+      :alt="blog.alt"
+      :src="blog.thumbnail"
+    >
+    </nuxt-img>
+    <h1>{blog.title}</h1>
+    <p>{blogAbstract}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+  props: {
+    blog: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    blogAbstract() {
+      return this.blog.content.substr(0, 150) + '...';
+    }
+  }
+};
+</script>
---
+++ store/modules/blog.js
@@ -0,0 +1,40 @@
+export default {
+  namespaced: true,
+  state() {
+    return {
+      data: {},
+    }
+  },
+  getters: {
+    data: (state) => state.data
+  },
+  actions: {
+    async fetch({ commit }) {
+      let photos = []
+      const {data, status } = await this.$axios.get("https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6fbbbf8a9d5ff41558c9100d42279af6&photoset_id=72177720305812757&user_id=184230567%40N04&format=json&nojsoncallback=1")
+      if (status === 200) {
+        data.photoset.photo.map((photo, index) => {
+          const item = {
+            index: index + 1,
+            enabled: true,
+            id: photo.id,
+            title: photo.title,
+            url: `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_c.jpg`
+          }
+          photos.push(item);
+        })
+        const fetchedData = await Promise.all(photos);
+        commit('SET_DATA', fetchedData)
+
+        return fetchedData;
+      }
+      photos.push({enabled: true, error: true, index: 1, title: 'no_image', url: '/no_image.png'})
+      commit('SET_DATA', photos )
+    },
+  },
+  mutations: {
+    SET_DATA(state, data) {
+      state.data = data;
+    },
+  },
+};
