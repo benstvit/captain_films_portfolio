df83f3175b6a4678089118c2a2f0f76d9681d3fd
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-05-11 17:30:32 +0200

    try to improve mixins

--- mixins/blogQuery.js
+++ mixins/blogQuery.js
@@ -1,37 +1,37 @@
 const query = `{
-  blogPostPhotoCollection(order: articleId_DESC) {
-    items {
-      articleId
-      title
-      thumbnail {
+    blogPostPhotoCollection(order: articleId_DESC) {
+      items {
+        articleId
         title
-        url
-      }
-      photoCredits
-      date
-      location {
-        lat
-        lon
-      }
-      introduction
-      question1
-      paragraph1
-      question2
-      paragraph2
-      question3
-      paragraph3
-      question4
-      paragraph4
-      imagesCollection {
-        items {
+        thumbnail {
           title
           url
-          width
-          height
+        }
+        photoCredits
+        date
+        location {
+          lat
+          lon
+        }
+        introduction
+        question1
+        paragraph1
+        question2
+        paragraph2
+        question3
+        paragraph3
+        question4
+        paragraph4
+        imagesCollection {
+          items {
+            title
+            url
+            width
+            height
+          }
         }
       }
     }
-  }
-}`
+  }`;
 
 export default query;
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -31,7 +31,6 @@ export default {
   },
   async mounted() {
     this.blogPosts = await this.getBlogPosts();
-    console.log(this.blogPosts);
   },
   methods: {
     async getBlogPosts() {
