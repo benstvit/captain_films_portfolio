bc713a758413552dc9c1148654caaaff4c48b6d8
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-05-13 16:36:00 +0200

    display of article + responsive design

--- components/pages/blog/post/PostContent.vue
+++ components/pages/blog/post/PostContent.vue
@@ -1,23 +1,48 @@
 <template>
   <div
-    class="relative flex flex-col items-center justify-center h-auto gap-2 w-1/2 mx-auto pt-8 font-cormorant text-lg text-justify"
+    class="flex flex-col items-center justify-center h-auto gap-2 w-full px-4 md:px-0 mx-auto md:w-1/2 pt-0 md:pt-8 font-cormorant text-base md:text-lg text-justify"
   >
     <p class="py-4" v-html="content(post.introduction)"></p>
     <nuxt-img
+          v-if="images[0]"
           preload
           format="webp"
-          class="w-full h-auto"
+          class="w-full h-auto my-2"
           :alt="images[0].title"
           :src="images[0].url"
         />
     <p :class="questionClass">{{ post.question1 }}</p>
-    <p v-html="content(post.paragraph1)"></p>
+    <p class="py-2" v-html="content(post.paragraph1)"></p>
+    <nuxt-img
+          v-if="images[1]"
+          preload
+          format="webp"
+          class="w-full h-auto my-2"
+          :alt="images[1].title"
+          :src="images[1].url"
+        />
     <p :class="questionClass">{{ post.question2 }}</p>
-    <p v-html="content(post.paragraph2)"></p>
+    <p class="py-2" v-html="content(post.paragraph2)"></p>
+    <nuxt-img
+          v-if="images[2]"
+          preload
+          format="webp"
+          class="w-full h-auto my-2"
+          :alt="images[2].title"
+          :src="images[2].url"
+        />
     <p :class="questionClass">{{ post.question3 }}</p>
-    <p v-html="content(post.paragraph3)"></p>
+    <p class="py-2" v-html="content(post.paragraph3)"></p>
+    <nuxt-img
+          v-if="images[3]"
+          preload
+          format="webp"
+          class="w-full h-auto my-2"
+          :alt="images[3].title"
+          :src="images[3].url"
+        />
     <p :class="questionClass">{{ post.question4 }}</p>
-    <p v-html="content(post.paragraph4)"></p>
+    <p class="py-2" v-html="content(post.paragraph4)"></p>
   </div>
 </template>
 
@@ -35,13 +60,13 @@ export default {
       return this.post.imagesCollection.items;
     },
     questionClass() {
-      console.log(this.post);
-      return "self-start font-playfair pt-4 overflow-visible";
+      return "self-start font-cormorant font-bold text-base md:text-xl italic pt-6 overflow-visible";
     },
   },
   methods: {
     content(text) {
       if (!text) return;
+      console.log(this.images);
 
       return this.$md.render(text);
     },
--- components/pages/blog/post/PostHeader.vue
+++ components/pages/blog/post/PostHeader.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="flex flex-col items-center gap-2 md:mx-20 pt-8">
+  <div class="flex flex-col items-center gap-2 mx-4 md:mx-20 pt-8">
       <h1 class="col-span-12 font-cormorant font-semibold p-4 text-3xl text-center">{{ post.title }}</h1>
       <div class="flex flex-col items-start font-cormorant font-light text-sm">
         <nuxt-img
           preload
           format="webp"
-          class="w-auto h-[70vh]"
+          class="w-full md:w-auto h-auto md:h-[70vh]"
           :alt="post.thumbnail.title"
           :src="post.thumbnail.url"
         />
-        <div class="flex justify-start items-center gap-2 py-2 w-full">
+        <div class="flex flex-col md:flex-row justify-start items-center gap-0 md:gap-2 py-2 w-full text-xs md:text-base">
           <p>{{date}}</p>
-          <p>|</p>
+          <p class="md:block hidden">|</p>
           <p>Tag: {{post.tag}}</p>
-          <p>|</p>
+          <p class="md:block hidden">|</p>
           <p>{{post.location}}</p>
-          <p class="ml-auto">{{post.photoCredits}}</p>
+          <p class="md:ml-auto">{{post.photoCredits}}</p>
         </div>
       </div>
     </div>
