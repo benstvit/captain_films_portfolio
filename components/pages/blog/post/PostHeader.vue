0477c00df78261e8b67031e8bcea6ba49a233928
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2023-05-13 16:56:34 +0200

    up

--- components/pages/blog/post/PostHeader.vue
+++ components/pages/blog/post/PostHeader.vue
@@ -1,40 +1,48 @@
 <template>
   <div class="flex flex-col items-center gap-2 mx-4 md:mx-20 pt-8">
-      <h1 class="col-span-12 font-cormorant font-semibold p-4 text-3xl text-center">{{ post.title }}</h1>
-      <div class="flex flex-col items-start font-cormorant font-light text-sm">
-        <nuxt-img
-          preload
-          format="webp"
-          class="w-full md:w-auto h-auto md:h-[70vh]"
-          :alt="post.thumbnail.title"
-          :src="post.thumbnail.url"
-        />
-        <div class="flex flex-col md:flex-row justify-start items-center gap-0 md:gap-2 py-2 w-full text-xs md:text-base">
-          <p>{{date}}</p>
-          <p class="md:block hidden">|</p>
-          <p>Tag: {{post.tag}}</p>
-          <p class="md:block hidden">|</p>
-          <p>{{post.location}}</p>
-          <p class="md:ml-auto">{{post.photoCredits}}</p>
-        </div>
+    <div
+      class="relative flex flex-col items-start font-cormorant font-light text-sm"
+    >
+      <h1
+        class="absolute top-2 md:top-4 right-2 left-2 text-center mx-auto font-cormorant font-semibold p-4 text-2xl md:text-4xl text-center text-white"
+      >
+        {{ post.title }}
+      </h1>
+      <nuxt-img
+        preload
+        format="webp"
+        class="w-full md:w-auto h-auto md:h-[70vh]"
+        :alt="post.thumbnail.title"
+        :src="post.thumbnail.url"
+      />
+      <div
+        class="flex flex-col md:flex-row justify-start items-start md:items-center gap-0 md:gap-2 py-2 w-full text-xs md:text-base"
+      >
+        <p>{{ date }}</p>
+        <p class="md:block hidden">|</p>
+        <p>Tag: {{ post.tag }}</p>
+        <p class="md:block hidden">|</p>
+        <p>{{ post.location }}</p>
+        <p class="md:ml-auto">{{ post.photoCredits }}</p>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'post-header',
+  name: "post-header",
   props: {
     post: {
       type: Object,
-      default: () => {}
-    }
+      default: () => {},
+    },
   },
   computed: {
     date() {
-      const date = new Date(this.post.date)
+      const date = new Date(this.post.date);
       return date.toLocaleDateString();
     },
-  }
-}
+  },
+};
 </script>
