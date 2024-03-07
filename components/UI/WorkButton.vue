dd2a5d8fecd6f0844c960938c4e2fc22a691e8d2
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2024-03-07 09:39:18 +0100

    create Work Button and implement on home page

---
+++ components/UI/WorkButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+    <nuxt-link
+      class="flex flex-col justify-center items-center gap-6" to="#">
+      <span
+        class="flex justify-center items-center w-16 h-16 lg:w-24 lg:h-24 rotate-45 bg-white border-2 hover:font-bold hover:border border-black transition ease-in-out duration-200"
+      >
+        <div
+          class="flex justify-center items-center w-14 h-14 lg:w-20 lg:h-20 transition ease-in-out opacity-90 hover:opacity-100 duration-200 border border-white hover:border-black bg-black hover:bg-white text-white hover:text-black hover:rotate-45 rounded-sm hover:shadow-lg hover:cursor-pointer hover:text-black"
+        >
+          <div
+            class="font-cormorant text-lg justify-self-start uppercase text-xs md:text-sm lg:text-lg -rotate-45 p-2"
+          >
+            Work
+          </div>
+        </div>
+      </span>
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Work Button',
+}
+</script>
